8422fe13dfb9bd5d81fa092615204dccb16a3d36
Harsh Patel <harshpatel29301@gmail.com>
2022-09-05 18:57:19 +0530

    all the content of the pages are fetching the data dynamically

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -116,7 +116,7 @@ const Slug = ({ variants, product, checkv }) => {
               height={300}
               width={300}
             /> */}
-              <img alt="ecommerce" class=" w-full lg:h-auto h-64 object-cover object-center rounded" src={product.img}></img>
+              <img alt="ecommerce" className=" w-full lg:h-auto h-64 object-cover object-center rounded" src={product.img}></img>
             </div>
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
               <h2 className="text-sm title-font text-gray-500 tracking-widest">
